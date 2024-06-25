db56dd1206705b1d15a639f08d9c52ac293c94c1
tc915 <taitcox0915@gmail.com>
2024-06-25 16:34:51 -0500

    updated demos page

--- client/src/components/Demos.jsx
+++ client/src/components/Demos.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../UserContext";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
@@ -42,7 +42,20 @@ const Demos = () => {
         head: false
     });
 
+    const [switchOnCount, setSwitchOnCount] = useState(0);
+    const [boatLength, setBoatLength] = useState(0);
+
     const lightTypes = Object.keys(lightStates)
+
+    useEffect(() => {
+        let newSwitchOnCount = 0
+        for (let i = 0; i < lightTypes.length; i++) {
+            if (lightStates[lightTypes[i]]) {
+                newSwitchOnCount++
+            }
+        }
+        setSwitchOnCount(newSwitchOnCount)
+    }, [lightStates])
 
     return (
         <motion.div className={`pt-32 h-screen ${darkMode ? 'bg-[#131313] text-white' : 'bg-white text-black'}`}
@@ -50,16 +63,30 @@ const Demos = () => {
             initial="initial"
             animate="animate"
         >
-            <div className="w-[30rem] h-full px-10 py-24 flex flex-wrap">
+            <div className="w-fit h-[5rem] p-4 relative">
+                <p>Boat Length</p>
+                <label className="absolute right-6 top-[57%]">ft</label>
+                <input type="number" placeholder="Boat length (ft.)" className={`outline-none border-2 px-2 py-1 pr-8 ${darkMode ? 'bg-transparent text-white border-white' : 'border-black'}`}
+                    onChange={(ev) => setBoatLength(ev.target.value)}
+                />
+            </div>
+
+            <div className="w-full px-12 py-12 flex flex-wrap">
+                <p className={`w-full text-2xl font-semibold mb-4 border-b-2 pb-2 ${darkMode ? 'border-white' : 'border-black'}`}>Lights</p>
                 {lightTypes.map((lightType) => (
                     <div className="flex mb-4" key={lightType}>
                         <p className="mr-4">{lightType.charAt(0).toUpperCase() + lightType.slice(1)} Lights</p>
-                        <button className={`border-2 border-black px-2 rounded-xl mr-4 ${lightStates[lightType] ? 'bg-blue-300' : ''}`}
+                        <button className={`border-2 px-2 rounded-xl mr-4 ${lightStates[lightType] ? 'bg-blue-300' : ''} ${darkMode ? 'border-white' : 'border-black'}`}
                             onClick={() => setLightStates(prev => ({ ...prev, [lightType]: !prev[lightType] }))}
                         >{lightStates[lightType] ? 'On' : 'Off'}</button>
                     </div>
                 ))}
+                <div className="w-full h-full mt-8">
+                    <p className="text-2xl font-semibold border-2 rounded-xl text-center py-2 mb-4">{`Boat Length: ${boatLength} ft`}</p>
+                    <p className="text-2xl font-semibold border-2 rounded-xl text-center py-2">{`Switch Count: ${switchOnCount} / ${lightTypes && lightTypes.length > 0 ? lightTypes.length : 0}`}</p>
+                </div>
             </div>
+
         </motion.div>
     );
 }
