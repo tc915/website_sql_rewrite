e54cc65a3ce2cfdd2a1625861de6ae27a7df485f
tc915 <taitcox0915@gmail.com>
2024-07-10 13:05:16 -0500

    Responsive design

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -339,7 +339,7 @@ const Products = () => {
             >
 
                 <div className='pt-[10rem] lgMobile:pt-[15rem] px-[22rem] lgMobile:px-6 flex justify-between items-end'>
-                    <h1 className="font-bold text-6xl text-center">Products</h1>
+                    <h1 className="font-bold text-6xl lgMobile:text-4xl lgMobile:mr-6 text-center">Products</h1>
                     <div className='flex items-center'>
                         <div className='text-2xl mr-2 font-md rounded-full w-[3rem] h-[3rem] bg-gradient-to-br from-[#FF7F11] to-[#facc22] text-white p-[3px] flex justify-center items-center'>
                             <div className='bg-[#131313] w-full h-full rounded-full'>
