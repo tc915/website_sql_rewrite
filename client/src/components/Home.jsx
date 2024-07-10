b1ca03cc5a89b3ed64bebbfddb2f6a5c0972ce96
tc915 <taitcox0915@gmail.com>
2024-07-10 10:29:44 -0500

    Responsive design

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -325,9 +325,9 @@ const Home = ({ scrollY }) => {
                             setThinkTankImageFilename(ev.target.files[0].name)
                         }}
                     />
-                    <div className="w-full flex items-center mt-4">
+                    <div className="w-full flex lgMobile:flex-col items-center mt-4">
                         <label htmlFor="imageInput" className="py-6 lgMobile:py-2 px-10 lgMobile:px-5 self-start ml-[8.3rem] lgMobile:ml-6 rounded-xl font-semibold text-xl lgMobile:text-lg border-4 border-[#FF7F11]">Choose Image</label>
-                        <p className="text-lg ml-4">{thinkTankImageFilename}</p>
+                        <p className="text-lg lgMobile:text-[1rem] ml-4">{thinkTankImageFilename}</p>
                     </div>
                     <button className="w-2/3 bg-[#FF7F11] text-white mt-10 py-2 rounded-full text-xl font-semibold"
                         onClick={() => saveThinkTankEdits()}
