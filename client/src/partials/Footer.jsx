1aa2a9cb56150e8140fa76b169092aac4c92ea48
tc915 <taitcox0915@gmail.com>
2024-07-15 10:20:11 -0500

    Responsive design

--- client/src/partials/Footer.jsx
+++ client/src/partials/Footer.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
-        <footer className="w-full lgMobile:w-[25rem] mdMobile:w-[25rem] h-[5rem] lgMobile:h-[15rem] mdMobile:h-[15rem] bg-black text-white font-semibold text-xl lgMobile:text-[0.8rem] mdMobile:text-[0.8rem] flex lgMobile:flex-col mdMobile:flex-col items-center lgMobile:items-start mdMobile:items-start p-6">
+        <footer className="w-full lgMobile:w-[25rem] mdMobile:w-[24rem] h-[5rem] lgMobile:h-[15rem] mdMobile:h-[15rem] bg-black text-white font-semibold text-xl lgMobile:text-[0.8rem] mdMobile:text-[0.8rem] flex lgMobile:flex-col mdMobile:flex-col items-center lgMobile:items-start mdMobile:items-start p-6">
             <p className="lgMobile:mb-2">Copyright © 2024 Interactive Technologies Inc. All Rights Reserved.</p>
             <div className="ml-auto lgMobile:ml-0 mdMobile:ml-0 lgMobile:flex mdMobile:flex lgMobile:flex-col mdMobile:flex-col">
                 <Link className="mr-6 lgMobile:mb-2 mdMobile:mb-2">Terms & Conditions</Link>
