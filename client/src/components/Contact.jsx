4da1f99bee4a3e2650f8f8194e58ce5b434a0432
tc915 <taitcox0915@gmail.com>
2024-07-15 11:57:25 -0500

    Responsive design

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -78,26 +78,26 @@ const Contact = ({ inAboutPage, setAboutPageEmailSentPopup }) => {
                     >Ok</button>
                 </div>
             </div>
-            <motion.div className="pt-44 lgMobile:pt-[15rem]"
+            <motion.div className="pt-44 lgMobile:pt-[15rem] mdMobile:pt-[15rem]"
                 variants={fadeInVariants}
                 initial="initial"
                 animate="animate"
             >
-                <img className="absolute -z-10 -translate-y-[15rem] lgMobile:-translate-y-[10rem] lgMobile:h-[50rem] object-cover" src="contact_background_unsplash.jpg" />
-                <div className="h-[48rem] flex lgMobile:flex-col text-white">
-                    <h2 className='lgMobile:mb-24 lgMobile:block hidden lgMobile:text-4xl lgMobile:font-semibold lgMobile:ml-6'>Contact Us</h2>
-                    <form className="w-1/2 lgMobile:w-full p-44 lgMobile:p-6 -mt-16"
+                <img className="absolute -z-10 -translate-y-[15rem] lgMobile:-translate-y-[10rem] mdMobile:-translate-y-[10rem] lgMobile:h-[50rem] mdMobile:h-[50rem] object-cover" src="contact_background_unsplash.jpg" />
+                <div className="h-[48rem] flex lgMobile:flex-col mdMobile:flex-col text-white">
+                    <h2 className='lgMobile:mb-24 mdMobile:mb-24 lgMobile:block mdMobile:block hidden lgMobile:text-4xl mdMobile:text-4xl lgMobile:font-semibold mdMobile:font-semibold lgMobile:ml-6 mdMobile:ml-6'>Contact Us</h2>
+                    <form className="w-1/2 lgMobile:w-full mdMobile:w-full p-44 lgMobile:p-6 mdMobile:p-6 -mt-16"
                         onSubmit={(ev) => {
                             ev.preventDefault();
                             sendMessage();
                         }}
                     >
                         <div className="w-full">
-                            <input type="text" required placeholder="Name" className="bg-transparent placeholder:text-white text-xl lgMobile:text-lg font-thin border-b-2 pb-2 lgMobile:pb-1 lgMobile:mb-8 mr-4 w-1/2 outline-none"
+                            <input type="text" required placeholder="Name" className="bg-transparent placeholder:text-white text-xl lgMobile:text-lg mdMobile:text-lg font-thin border-b-2 pb-2 lgMobile:pb-1 mdMobile:pb-1 lgMobile:mb-8 mdMobile:mb-8 mr-4 w-1/2 outline-none"
                                 value={name}
                                 onChange={(ev) => setName(ev.target.value)}
                             />
-                            <input type='text' placeholder="Phone (not required)" className="bg-transparent placeholder:text-white text-xl lgMobile:text-lg font-thin border-b-2 pb-2 lgMobile:pb-1 outline-none"
+                            <input type='text' placeholder="Phone (not required)" className="bg-transparent placeholder:text-white text-xl lgMobile:text-lg mdMobile:text-lg font-thin border-b-2 pb-2 lgMobile:pb-1 mdMobile:pb-1 outline-none"
                                 value={phoneNum}
                                 onChange={(ev) => setPhoneNum(ev.target.value)}
                             />
@@ -120,11 +120,11 @@ const Contact = ({ inAboutPage, setAboutPageEmailSentPopup }) => {
                             whileTap="click"
                         >Send</motion.button>
                     </form>
-                    <div className="text-8xl flex justify-center items-center -mt-44 lgMobile:hidden">
+                    <div className="text-8xl flex justify-center items-center -mt-44 lgMobile:hidden mdMobile:hidden">
                         <p className="w-2/3">Let's Work Together!</p>
                     </div>
                 </div>
-                <div className='lgMobile:-mt-32'>
+                <div className='lgMobile:-mt-32 mdMobile:-mt-32'>
                     <Footer />
                 </div>
             </motion.div>
