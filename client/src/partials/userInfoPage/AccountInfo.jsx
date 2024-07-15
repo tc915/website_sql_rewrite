b7db1481e8a48b35506011967f7afd049a794265
tc915 <taitcox0915@gmail.com>
2024-07-15 12:46:50 -0500

    Responsive design

--- client/src/partials/userInfoPage/AccountInfo.jsx
+++ client/src/partials/userInfoPage/AccountInfo.jsx
@@ -37,22 +37,22 @@ const AccountInfo = ({ name, setName, email, setEmail, password, setPassword, da
     }
 
     return (
-        <form className="w-[65rem] lgMobile:px-0 lgMobile:mt-16 flex flex-col items-center px-24 lgMobile:ml-28"
+        <form className="w-[65rem] lgMobile:px-0 mdMobile:px-0 lgMobile:mt-16 mdMobile:mt-16 flex flex-col items-center px-24 lgMobile:ml-28 mdMobile:ml-28"
             onSubmit={(ev) => {
                 ev.preventDefault();
                 saveAccountChanges();
             }}
         >
-            <h1 className="mt-[6rem] font-bold text-3xl lgMobile:text-xl">Account Info</h1>
-            <p className="mt-6 text-2xl lgMobile:text-lg font-semibold">{name}</p>
-            <div className="w-full lgMobile:w-1/2 h-[5rem] mt-6 flex items-center px-24 lgMobile:px-4 rounded-2xl border-2">
+            <h1 className="mt-[6rem] font-bold text-3xl lgMobile:text-xl mdMobile:text-xl">Account Info</h1>
+            <p className="mt-6 text-2xl lgMobile:text-lg mdMobile:text-lg font-semibold">{name}</p>
+            <div className="w-full lgMobile:w-1/2 mdMobile:w-1/2 h-[5rem] mt-6 flex items-center px-24 lgMobile:px-4 mdMobile:px-4 rounded-2xl border-2">
                 <label htmlFor="userEmail" className="text-xl font-semibold mr-4 lgMobile:hidden">Email:</label>
-                <p className={`text-xl lgMobile:hidden font-semibold ${darkMode ? 'text-white/50' : 'text-black/40'} ${isChangeEmail ? 'hidden' : ''}`}>{email || ''}</p>
+                <p className={`text-xl lgMobile:hidden mdMobile:hidden font-semibold ${darkMode ? 'text-white/50' : 'text-black/40'} ${isChangeEmail ? 'hidden' : ''}`}>{email || ''}</p>
                 <input type="email" name="userEmail" id="userEmail" placeholder="Email" required autoComplete="email" className={`border-b-2 p-2 font-semibold outline-none bg-transparent ${isChangeEmail ? '' : 'hidden'}`}
                     value={email || ''}
                     onChange={(ev) => setEmail(ev.target.value)}
                 />
-                <button className={`${isChangeEmail ? 'hidden' : ''} border-4 border-[#FF7F11] px-10 py-2 text-xl lgMobile:text-sm ${darkMode ? 'text-white' : 'text-black'} font-semibold rounded-full ml-auto`}
+                <button className={`${isChangeEmail ? 'hidden' : ''} border-4 border-[#FF7F11] px-10 py-2 text-xl lgMobile:text-sm mdMobile:text-sm ${darkMode ? 'text-white' : 'text-black'} font-semibold rounded-full ml-auto`}
                     onClick={(ev) => {
                         ev.preventDefault();
                         setIsChangeEmail(true);
@@ -67,9 +67,9 @@ const AccountInfo = ({ name, setName, email, setEmail, password, setPassword, da
                     <svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill={darkMode ? '#fff' : '#131313'}><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" /></svg>
                 </button>
             </div>
-            <div className="w-full lgMobile:w-1/2 h-[5rem] mt-6 flex lgMobile:flex-col lgMobile:justify-center items-center px-24 lgMobile:px-4 rounded-2xl border-2">
-                <label htmlFor="userPassword" className="text-xl font-semibold mr-4 lgMobile:hidden">Password:</label>
-                <button className={`${isChangePassword ? 'hidden' : ''} border-4 border-[#FF7F11] px-6 rounded-full text-xl lgMobile:text-sm font-semibold ${darkMode ? 'text-white' : 'text-black'} py-2 ml-auto`}
+            <div className="w-full lgMobile:w-1/2 mdMobile:w-1/2 h-[5rem] mt-6 flex lgMobile:flex-col mdMobile:flex-col lgMobile:justify-center mdMobile:justify-center items-center px-24 lgMobile:px-4 mdMobile:px-4 rounded-2xl border-2">
+                <label htmlFor="userPassword" className="text-xl font-semibold mr-4 lgMobile:hidden mdMobile:hidden">Password:</label>
+                <button className={`${isChangePassword ? 'hidden' : ''} border-4 border-[#FF7F11] px-6 rounded-full text-xl lgMobile:text-sm mdMobile:text-sm font-semibold ${darkMode ? 'text-white' : 'text-black'} py-2 ml-auto`}
                     onClick={(ev) => {
                         ev.preventDefault();
                         setIsChangePassword(true)
