54ed8062e42ee614c7f5c4a4bba385f3168c9a1c
tc915 <taitcox0915@gmail.com>
2024-07-29 09:20:44 -0500

    Responsive design

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -248,7 +248,7 @@ const Products = () => {
                                             setNewProductVariablePricing(updatedPricing);
                                         }}
                                     >
-                                        <svg xmlns="https://www.w3.org/2000/svg" className='w-[36px] h-[36px] lgMobile:w-[24px] mdMobile:w-[24px] lgMobile:h-[24px] mdMobile:h-[24px]' viewBox="0 -960 960 960" fill="#fff"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" /></svg>
+                                        <svg xmlns="https://www.w3.org/2000/svg" className='w-[36px] h-[36px] tablet:w-[32px] tablet:h-[32px] lgMobile:w-[24px] mdMobile:w-[24px] lgMobile:h-[24px] mdMobile:h-[24px]' viewBox="0 -960 960 960" fill="#fff"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" /></svg>
                                     </button>
                                 </div>
                             ))}
