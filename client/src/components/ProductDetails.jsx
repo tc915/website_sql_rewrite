9b6688091a4b8de55827ccab1e9a27e55a3b978d
tc915 <taitcox0915@gmail.com>
2024-07-29 10:17:21 -0500

    Responsive design

--- client/src/components/ProductDetails.jsx
+++ client/src/components/ProductDetails.jsx
@@ -154,7 +154,7 @@ const ProductDetails = () => {
 
 
     return (
-        <div className={`pt-40 tablet:py-32 lgMobile:py-56 mdMobile:py-56 lgMobile:pb-24 mdMobile:pb-24 ${darkMode ? 'text-white bg-[#131313]' : 'text-black bg-white'}`}>
+        <div className={`pt-40 tablet:pt-32 lgMobile:py-56 mdMobile:py-56 lgMobile:pb-24 mdMobile:pb-24 ${darkMode ? 'text-white bg-[#131313]' : 'text-black bg-white'}`}>
             <div className="w-full h-[40rem] p-10 tablet:p-2 flex lgMobile:flex-col mdMobile:flex-col relative px-[15rem] lgMobile:px-0 mdMobile:px-0 mb-44">
                 {adminPrivileges && (
                     <button className="absolute top-4 tablet:-top-4 right-[17rem] tablet:right-4 px-10 border-[3px] border-gray-300 rounded-xl font-semibold"
