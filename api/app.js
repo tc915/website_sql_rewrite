5f0b1a273ee2ff3cece4b19db6ef51ff7c29921c
tc915 <taitcox0915@gmail.com>
2024-08-07 13:26:44 -0500

    Stripe implementation

--- api/app.js
+++ api/app.js
@@ -759,6 +759,7 @@ app.post('/webhook', webhook)
 app.get('/:sessionId', (req, res) => {
     try {
         const { sessionId } = req.params
+        console.log(sessionsStore)
         const session = sessionsStore
         if (session) {
             res.status(200).json(session)
